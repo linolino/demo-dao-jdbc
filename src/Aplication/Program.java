04c8c6d9c3d5c0c1bc51484fc53c3badafe13bdd
Laudelino Coriolano da Mata <nene.mata@gmail.com>
2026-03-18 01:59:33 -0300

    Seller entity class

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,13 +1,25 @@
 package Aplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1,"Books");
+		
+		Seller seller =new Seller(1,"Paulo","paulo@gmail.com",new Date(),3000.00,obj);
+				
+				
+				
 
 		System.out.println(obj);
+		System.out.println(seller);
 	}
 
 }
