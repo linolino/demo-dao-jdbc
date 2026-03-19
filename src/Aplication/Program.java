b0bdf63a4863e0d7cec1a5e0644e63c509f5dd20
Laudelino Coriolano da Mata <nene.mata@gmail.com>
2026-03-19 18:49:03 -0300

    findById implementation

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,28 +1,15 @@
 package Aplication;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-		Department obj = new Department(1,"Books");
-		
-		Seller seller =new Seller(1,"Paulo","paulo@gmail.com",new Date(),3000.00,obj);
-				
-		SellerDao sellerDao  = DaoFactory.createSellerDao();
-		
-				
 
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findyId(3);
 		System.out.println(seller);
 	}
 
