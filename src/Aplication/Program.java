8aeb4437b6127fd7315a70e4a7acd21c424ef56b
Laudelino Coriolano da Mata <nene.mata@gmail.com>
2026-03-19 17:41:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -3,6 +3,8 @@ package Aplication;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,7 +18,8 @@ public class Program {
 		
 		Seller seller =new Seller(1,"Paulo","paulo@gmail.com",new Date(),3000.00,obj);
 				
-				
+		SellerDao sellerDao  = DaoFactory.createSellerDao();
+		
 				
 
 		System.out.println(obj);
